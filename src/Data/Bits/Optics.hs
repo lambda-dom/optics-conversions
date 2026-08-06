350101e69449ffe464901eed5db97f32c833e3e7
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2026-08-06 20:27:09 +0100

    Refactor using safe optics as much as possible. (#5)
    * Refactor using safe optics as much as possible.
    
    * Two isomorphisms for `Word64`.
    
    * INLINE pragmas.

--- src/Data/Bits/Optics.hs
+++ src/Data/Bits/Optics.hs
@@ -20,16 +20,21 @@ module Data.Bits.Optics (
     word16BytesBe,
     word32BytesLe,
     word32BytesBe,
+    word64BytesLe,
+    word64BytesBe,
 ) where
 
 
 -- Imports.
 -- Base.
 import Data.Bits (Bits (..), FiniteBits (..))
-import Data.Word (Word8, Word16, Word32)
+import Data.Word (Word8, Word16, Word32, Word64)
 
 -- Libraries.
-import Optics.Core (Lens', Iso', lens, iso, view, set)
+import Optics.Core (Lens', Iso', lens, iso, view, set, review)
+
+-- Package.
+import Data.Word.Optics (word8ToWord16, word8ToWord32, word8ToWord64)
 
 
 -- $setup
@@ -77,7 +82,7 @@ note(s):
     * The result is undefined if the argument is negative or larger than the number of bits in the
     type.
 -}
-{-# INLINEABLE bitAt #-}
+{-# INLINE bitAt #-}
 bitAt :: forall a . Bits a => Int -> Lens' a Bool
 bitAt i = lens project update
     where
@@ -95,7 +100,7 @@ note(s):
     * The result is undefined if the argument is negative or larger than the number of bytes in the
     type.
 -}
-{-# INLINEABLE byteAt #-}
+{-# INLINE byteAt #-}
 byteAt :: forall a . (Integral a, Bits a) => Int -> Lens' a Word8
 byteAt i = lens project update
     where
@@ -117,7 +122,7 @@ note(s):
 >>> view bitsLe 0x04
 (False,False,True,False,False,False,False,False)
 -}
-{-# INLINEABLE bitsLe #-}
+{-# INLINE bitsLe #-}
 bitsLe :: Iso' Word8 (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
 bitsLe = iso from to
     where
@@ -158,7 +163,7 @@ note(s):
 >>> view bitsBe 0x04
 (False,False,False,False,False,True,False,False)
 -}
-{-# INLINEABLE bitsBe #-}
+{-# INLINE bitsBe #-}
 bitsBe :: Iso' Word8 (Bool, Bool, Bool, Bool, Bool, Bool, Bool, Bool)
 bitsBe = iso from to
     where
@@ -199,7 +204,7 @@ note(s):
 >>> view word16BytesLe 0xff00
 (0,255)
 -}
-{-# INLINEABLE word16BytesLe #-}
+{-# INLINE word16BytesLe #-}
 word16BytesLe :: Iso' Word16 (Word8, Word8)
 word16BytesLe = iso from to
     where
@@ -207,7 +212,7 @@ word16BytesLe = iso from to
         from n = (view (byteAt 0) n, view (byteAt 1) n)
 
         to :: (Word8, Word8) -> Word16
-        to (m, n) = fromIntegral m .|. shiftL (fromIntegral n) 8
+        to (m, n) = let h = review word8ToWord16 in h m .|. shiftL (h n) 8
 
 {- | Isomorphism between 'Word16' and tuples of 'Word8'.
 
@@ -220,7 +225,7 @@ note(s):
 >>> view word16BytesBe 0xff00
 (255,0)
 -}
-{-# INLINEABLE word16BytesBe #-}
+{-# INLINE word16BytesBe #-}
 word16BytesBe :: Iso' Word16 (Word8, Word8)
 word16BytesBe = iso from to
     where
@@ -228,7 +233,7 @@ word16BytesBe = iso from to
         from n = (view (byteAt 1) n, view (byteAt 0) n)
 
         to :: (Word8, Word8) -> Word16
-        to (m, n) = fromIntegral n .|. shiftL (fromIntegral m) 8
+        to (m, n) = let h = review word8ToWord16 in h n .|. shiftL (h m) 8
 
 {- | Isomorphism between 'Word32' and 4-tuples of 'Word8'.
 
@@ -241,7 +246,7 @@ note(s):
 >>> view word32BytesLe 0xff00
 (0,255,0,0)
 -}
-{-# INLINEABLE word32BytesLe #-}
+{-# INLINE word32BytesLe #-}
 word32BytesLe :: Iso' Word32 (Word8, Word8, Word8, Word8)
 word32BytesLe = iso from to
     where
@@ -249,11 +254,11 @@ word32BytesLe = iso from to
         from n = (view (byteAt 0) n, view (byteAt 1) n, view (byteAt 2) n, view (byteAt 3) n)
 
         to :: (Word8, Word8, Word8, Word8) -> Word32
-        to (m, n, o, p)
-            =   fromIntegral m
-            .|. shiftL (fromIntegral n) 8
-            .|. shiftL (fromIntegral o) 16
-            .|. shiftL (fromIntegral p) 24
+        to (m, n, o, p) = let h = review word8ToWord32 in
+                h m
+            .|. shiftL (h n) 8
+            .|. shiftL (h o) 16
+            .|. shiftL (h p) 24
 
 {- | Isomorphism between 'Word32' and 4-tuples of 'Word8'.
 
@@ -266,7 +271,7 @@ note(s):
 >>> view word32BytesBe 0xff00
 (0,0,255,0)
 -}
-{-# INLINEABLE word32BytesBe #-}
+{-# INLINE word32BytesBe #-}
 word32BytesBe :: Iso' Word32 (Word8, Word8, Word8, Word8)
 word32BytesBe = iso from to
     where
@@ -274,8 +279,84 @@ word32BytesBe = iso from to
         from n = (view (byteAt 3) n, view (byteAt 2) n, view (byteAt 1) n, view (byteAt 0) n)
 
         to :: (Word8, Word8, Word8, Word8) -> Word32
-        to (m, n, o, p)
-            =   fromIntegral p
-            .|. shiftL (fromIntegral o) 8
-            .|. shiftL (fromIntegral n) 16
-            .|. shiftL (fromIntegral m) 24
+        to (m, n, o, p) =  let h = review word8ToWord32 in
+                h p
+            .|. shiftL (h o) 8
+            .|. shiftL (h n) 16
+            .|. shiftL (h m) 24
+
+{- | Isomorphism between 'Word64' and 8-tuples of 'Word8'.
+
+note(s):
+
+    * The order of the bytes is little-endian, that is, least to most significant.
+
+=== __Examples:__
+
+>>> view word64BytesLe 0xff00
+(0,255,0,0,0,0,0,0)
+-}
+{-# INLINE word64BytesLe #-}
+word64BytesLe :: Iso' Word64 (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8)
+word64BytesLe = iso from to
+    where
+        from :: Word64 -> (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8)
+        from n = (
+                    view (byteAt 0) n,
+                    view (byteAt 1) n,
+                    view (byteAt 2) n,
+                    view (byteAt 3) n,
+                    view (byteAt 4) n,
+                    view (byteAt 5) n,
+                    view (byteAt 6) n,
+                    view (byteAt 7) n
+                )
+
+        to :: (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8) -> Word64
+        to (m, n, o, p, q, r, s, t) = let h = review word8ToWord64 in
+                h m
+            .|. shiftL (h n) 8
+            .|. shiftL (h o) 16
+            .|. shiftL (h p) 24
+            .|. shiftL (h q) 32
+            .|. shiftL (h r) 40
+            .|. shiftL (h s) 48
+            .|. shiftL (h t) 56
+
+{- | Isomorphism between 'Word64' and 8-tuples of 'Word8'.
+
+note(s):
+
+    * The order of the bytes is big-endian, that is, most to least significant.
+
+=== __Examples:__
+
+>>> view word64BytesBe 0xff00
+(0,0,0,0,0,0,255,0)
+-}
+{-# INLINE word64BytesBe #-}
+word64BytesBe :: Iso' Word64 (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8)
+word64BytesBe = iso from to
+    where
+        from :: Word64 -> (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8)
+        from n = (
+                    view (byteAt 7) n,
+                    view (byteAt 5) n,
+                    view (byteAt 5) n,
+                    view (byteAt 4) n,
+                    view (byteAt 3) n,
+                    view (byteAt 2) n,
+                    view (byteAt 1) n,
+                    view (byteAt 0) n
+                )
+
+        to :: (Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8) -> Word64
+        to (m, n, o, p, q, r, s, t) =  let h = review word8ToWord64 in
+                h t
+            .|. shiftL (h s) 8
+            .|. shiftL (h r) 16
+            .|. shiftL (h q) 24
+            .|. shiftL (h p) 32
+            .|. shiftL (h o) 40
+            .|. shiftL (h n) 48
+            .|. shiftL (h m) 56
